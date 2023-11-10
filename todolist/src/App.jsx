ef2c49b66aa6d4cf4cb5ffb9d05b6c61e63f1e21
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-09 21:19:35 -0300

    Update App.jsx

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -23,7 +23,23 @@ function App() {
   ])
 
   return <div className='app'>
-    Olá
+    <h1>Lista de Tarefas</h1>
+    <div className='todo-list'>
+      {todos.map((todo) => (
+        <div className='todo'>
+          <div className='content'>
+            <p>{todo.text}</p>
+            <p className='category'>
+              ({todo.category})
+            </p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>x</button>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>;
 }
 
