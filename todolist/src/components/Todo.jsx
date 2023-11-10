ae7b731644b8963719e771d7a3dc1c7f5098a262
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-09 22:13:25 -0300

    up

--- todolist/src/components/Todo.jsx
+++ todolist/src/components/Todo.jsx
@@ -1,8 +1,16 @@
 // diita rafce para extensao fazer o trabalho
 
-import React from 'react'
+import { useState } from 'react'
 
 const Todo = ({todo}) => {
+    const[value, setValue] = useState("");
+    const[category, setCategory] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDeFault();
+        console.log("Enviou form!")
+    }
+
   return (
     <div className='todo'>
     <div className='content'>
@@ -12,8 +20,8 @@ const Todo = ({todo}) => {
       </p>
     </div>
     <div>
-      <button>Completar</button>
-      <button>x</button>
+      <button className='complete'>Completar</button>
+      <button className='remove'>x</button>
     </div>
   </div>
   )
