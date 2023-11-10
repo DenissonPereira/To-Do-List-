354186c1ce93fe7f0dd0633e8a020bee5c3220fa
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-09 21:10:29 -0300

    Update App.jsx

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,8 +1,30 @@
 import { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "work",
+      iscompleted: false,
+    },
+    {
+      id: 2,
+      text: "Criar funcionalidade y no sistema",
+      category: "Pessoal",
+      iscompleted: false,
+    },
+    {
+      id: 3,
+      text: "Criar funcionalidade z no sistema",
+      category: "Estudos",
+      iscompleted: false,
+    },
+  ])
 
-  return <div>Olá, Reat!</div>;
+  return <div className='app'>
+    Olá
+  </div>;
 }
 
 export default App
